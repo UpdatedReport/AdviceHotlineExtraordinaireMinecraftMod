403519e5f2ecdf335489b3e53f39bdb20f94a3db
UpdatedReport <updatedreportgames@gmail.com>
2026-05-22 23:13:22 +0900

    - Added Everything Man Coin

--- src/main/java/updatedreport/advicehotline/AdviceHotlineExtraordinaire.java
+++ src/main/java/updatedreport/advicehotline/AdviceHotlineExtraordinaire.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import updatedreport.advicehotline.item.AdviceHotlineItem;
 
 public class AdviceHotlineExtraordinaire implements ModInitializer {
 	public static final String MOD_ID = "advicehotline";
@@ -12,7 +13,7 @@ public class AdviceHotlineExtraordinaire implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+        AdviceHotlineItem.registerModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/updatedreport/advicehotline/item/AdviceHotlineItem.java
@@ -0,0 +1,26 @@
+package updatedreport.advicehotline.item;
+
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
+import updatedreport.advicehotline.AdviceHotlineExtraordinaire;
+
+import java.util.function.Function;
+
+public class AdviceHotlineItem {
+    public static final Item EVERYTHING_MAN_COIN = registerItem("everything_man_coin", Item::new);
+
+
+    private static Item registerItem(String name, Function<Item.Properties, Item> function) {
+        return Registry.register(BuiltInRegistries.ITEM, Identifier.fromNamespaceAndPath(AdviceHotlineExtraordinaire.MOD_ID, name),
+                function.apply(new Item.Properties().setId(ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(AdviceHotlineExtraordinaire.MOD_ID, name)))));
+    }
+
+    public static void registerModItems() {
+        AdviceHotlineExtraordinaire.LOGGER.info("Registering mod items for " + AdviceHotlineExtraordinaire.MOD_ID);
+    }
+
+}
